e1f529182723f255d823dcae896e95d89423ae1f
zehiu8529 <zehiu8529@gmail.com>
2022-08-21 21:55:14 +0700

    Add assets

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,20 +8,25 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private float speed = 360;
     private Rigidbody2D rb;
+    SoundHandler soundHandler;
     // Start is called before the first frame update
     void Start()
     {
+        soundHandler = GetComponent<SoundHandler>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            SoundHandler.PlayWalkingSound();
+        }
     }
     private void FixedUpdate()
     {
-        Move();
+        Move();        
     }
 
     void Move()
@@ -30,6 +35,6 @@ public class Movement : MonoBehaviour
         float inputY = Input.GetAxisRaw("Vertical");
         float moveX = inputX * Time.fixedDeltaTime;
         float moveY = inputY * Time.fixedDeltaTime;
-        rb.velocity = new Vector2(moveX * speed, moveY * speed);
+        rb.velocity = new Vector2(moveX * speed, moveY * speed);        
     }
 }
